b4b2601f5a8b5e538cab7727b8e99cd8c8adb7d8
Suyash Pundir <125992173+suyashpundir@users.noreply.github.com>
2026-05-05 12:05:44 +0530

    Update and rename script.py to scanner.py

---
+++ scanner.py
@@ -0,0 +1,9 @@
+import boto3
+
+def check_resources():
+    print("CI/CD Pipeline: Checking for cloud waste...")
+    # This is a placeholder for your future logic
+    return True
+
+if __name__ == "__main__":
+    check_resources()
